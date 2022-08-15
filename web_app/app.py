c6764d7b581a6fc1cdb260393a701faa46338bbf
velatkilic <velatkilic@gmail.com>
2022-08-15 19:20:25 -0400

    added abstract topic modelling

--- web_app/app.py
+++ web_app/app.py
@@ -1,11 +1,35 @@
 import utils
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # load and precompute stuff
 utils.init_model()
 
 # st app
 st.title("CVPR Text Analysis and Recommendation")
+
+# show qr code
+_, qr_col, _ = st.columns(3)
+with qr_col:
+    qr = plt.imread("github_link.png")
+    st.image(qr, caption="Github Link", width=200)
+
+st.write("[https://github.com/velatkilic/CVPR_Paper_Analysis_NLP](https://github.com/velatkilic/CVPR_Paper_Analysis_NLP)")
+
+# customer persona
+with st.expander(label="Customer Needs"):
+    img = plt.imread("customer_persona.png")
+    st.image(img, "Customer Needs")
+
+# show data sources
+with st.expander(label="Data Sources"):
+    img = plt.imread("data_sources.png")
+    st.image(img, caption="Data Sources")
+
+# show topics
+with st.expander(label="Topic Modelling"):
+    img = plt.imread("title_topics.png")
+    st.image(img, caption="Topic Modelling with Titles")
 
 with st.form(key="topic-search"):
     c1, c2= st.columns(2)
@@ -44,7 +68,42 @@ with st.form(key="topic-search"):
         else:
             st.write("Text not in the vocabulary")
 
+with st.form(key="topic-search-abstract"):
+    c1, c2= st.columns(2)
+    with c1:
+        text_input = st.text_input("Search Abstract")
+    with c2:
+        n_paper = st.number_input("Number of papers", 1, 100, 5)
+    
+    c1_opt, c2_opt = st.columns(2)
+    
+    with c1_opt:
+        sopt = st.radio("Sort by", ("citation", "year", "author"), index=1)
+    with c2_opt:
+        check_box = st.checkbox('Include Preview URLs')
+    
 
+    sopt_dict = {"citation": "citation_count" , "year":"year" ,"author":"authors"}
+
+    rec_paper = st.form_submit_button(label = 'Recommend Papers')
+
+    if rec_paper:
+        out = utils.get_relevant_papers_abstract(text_input, n_paper)
+        if out is not None:
+            # sort
+            if sopt_dict[sopt] in ["citation_count", "year"]:
+                out = out.sort_values(by=[sopt_dict[sopt]], ascending=False)
+            else:
+                out = out.sort_values(by=[sopt_dict[sopt]])
+
+            # show data
+            if check_box:
+                out['link'] = out['link'].apply(utils.make_clickable, args = ('PDF Link',))
+                st.write(out[["authors", "title", "year", "abstract", "link", "citation_count"]].to_html(escape = False), unsafe_allow_html = True)
+            else:
+                st.dataframe(out[["authors", "title", "year", "abstract", "link", "citation_count"]])
+        else:
+            st.write("Text not in the vocabulary")
 
 with st.form(key="topics"):
     c1, c2 = st.columns(2)
@@ -70,7 +129,7 @@ with st.form(key="topics"):
 with st.form(key="language-model"):
     c1_lm, c2_lm = st.columns((5,1))
     with c1_lm:
-        abs_input = st.text_area("Enter text for word suggestions")
+        abs_input = st.text_area("Enter text for word suggestions", value = "To this end, we design a deep latent space deformation network that is directly parameterized by the kernel. The network consists of three components: encoder, deformer, and decoder, where the deformer is specifically meant to rectify the latent space representations of blurred images to a standard latent space, regardless of the kernel. The deformation network is trained with a two-stage training scheme. We conduct extensive experiments to confirm that our parametric model can adapt to drastically different blurring kernels and perform robust deblurring.")
     with c2_lm:
         n_words = st.number_input("Number of suggestions", 1, 100, 10)
     
--- web_app/utils.py
+++ web_app/utils.py
@@ -12,6 +12,14 @@ def get_topic_names(n=3):
         topic_names.append(top_features)
     return topic_names
 
+def get_topic_names_abstract(n=3):
+    topic_names = []
+    for _, topic in enumerate(nmf_abstract.components_):
+        top_features_ind = topic.argsort()[: -n - 1 : -1]
+        top_features = [tfidf_feature_names_abstract[i] for i in top_features_ind]
+        topic_names.append(top_features)
+    return topic_names
+
 def find_closest_match(topic_embed, n=5):
     # find the closest match
     dist = (topic_vector - topic_embed)**2
@@ -24,6 +32,17 @@ def find_closest_match(topic_embed, n=5):
 
     return df.iloc[inds, :]
 
+def find_closest_match_abstract(topic_embed, n=5):
+    # find the closest match
+    dist = (topic_vector_abstract - topic_embed)**2
+    dist = dist.mean(axis=1)
+    print(dist.shape)
+
+    # find the best match
+    inds = dist.argsort()[0:n]
+    print(inds.shape)
+
+    return df.iloc[inds, :]
 
 def get_relevant_papers(input_text, n=5):
 
@@ -41,6 +60,23 @@ def get_relevant_papers(input_text, n=5):
     out = find_closest_match(topic_embed, n)
 
     return out #out["title"].to_json()
+
+def get_relevant_papers_abstract(input_text, n=5):
+
+    # tfidf vectorizer
+    input_tfidf = tfidf_vectorizer_abstract.transform([input_text])
+
+    # check at least one word is in the vocab
+    if input_tfidf.count_nonzero() == 0:
+        return None
+
+    # get topic embedding
+    topic_embed = nmf_abstract.transform(input_tfidf)
+    
+    # closest match
+    out = find_closest_match_abstract(topic_embed, n)
+
+    return out
 
 def get_top_by_count(n=5):
     global top_topic_inds
@@ -76,23 +112,34 @@ def make_clickable(url, text):
 
 
 def get_word_suggestions(abs_input, top_k):
+    abs_input = abs_input.replace("#", "[MASK]")
     return tf.fill_mask(abs_input, top_k=top_k)
 
 def init_model(model_dir = "..\\model", lm_dir="distilbert-base-uncased-finetuned-cvpr"):
     
-    global nmf, tfidf_vectorizer, df, tfidf_feature_names, topic_vector
+    global nmf, nmf_abstract, tfidf_vectorizer, tfidf_vectorizer_abstract, df, tfidf_feature_names, tfidf_feature_names_abstract, topic_vector, topic_vector_abstract
     with open(model_dir + "\\nmf.pickle", "rb") as f:
         nmf = pickle.load(f)
+    
+    with open(model_dir + "\\nmf_abstract.pickle", "rb") as f:
+        nmf_abstract = pickle.load(f)
     
     with open(model_dir + "\\tfidf_vectorizer.pickle", "rb") as f:
         tfidf_vectorizer = pickle.load(f)
     
+    with open(model_dir + "\\tfidf_vectorizer_abstract.pickle", "rb") as f:
+        tfidf_vectorizer_abstract = pickle.load(f)
+    
     with open(model_dir + "\\topic_vector.pickle", "rb") as f:
         topic_vector = pickle.load(f)
+    
+    with open(model_dir + "\\topic_vector_abstract.pickle", "rb") as f:
+        topic_vector_abstract = pickle.load(f)
     
     df = pd.read_csv(model_dir + "\\cvpr_data_with_topics.csv")
 
     tfidf_feature_names = tfidf_vectorizer.get_feature_names_out()
+    tfidf_feature_names_abstract = tfidf_vectorizer_abstract.get_feature_names_out()
 
     df["topic_class"] = topic_vector.argmax(axis=1)
 
